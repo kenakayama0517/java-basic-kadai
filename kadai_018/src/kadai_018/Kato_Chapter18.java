89c9b9e74295d36bb597ec544756a8926b428494
kenakayama0517 <keiju.nakayama@y-yokohama.com>
2026-04-20 16:06:04 +0900

    Add files via upload

---
+++ kadai_018/src/kadai_018/Kato_Chapter18.java
@@ -0,0 +1,25 @@
+package kadai_018;
+
+public abstract class Kato_Chapter18  {
+	public String familyName = "加藤"; //姓を表す
+	public String givenName;  //名を表す
+	public String address = "東京都中野区〇×"; //住所を表す	
+
+
+	public void commonIntroduce() {
+		//共通の紹介を出力する
+		System.out.println("名前は"+familyName + givenName + "です");
+		System.out.println("住所は"+address+"です");
+	}
+
+	public abstract void eachIntroduce();
+		//個別の紹介を出力する
+
+
+	public void execIntroduce() {
+		//紹介を実行する
+		commonIntroduce();
+		eachIntroduce();
+		System.out.println();
+	}
+}
