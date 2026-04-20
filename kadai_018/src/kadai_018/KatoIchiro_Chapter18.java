4d1f213747f51629cd6207c39fb15cb2455fce5f
kenakayama0517 <keiju.nakayama@y-yokohama.com>
2026-04-20 15:24:23 +0900

    Add files via upload

---
+++ kadai_018/src/kadai_018/KatoIchiro_Chapter18.java
@@ -0,0 +1,15 @@
+package kadai_018;
+
+
+public class KatoIchiro_Chapter18 extends Kato_Chapter18{
+		
+	public void setGivenName() {
+		this.givenName = "一郎";
+		//名を表すフィールドの値をセットする		
+	}
+	
+		
+	public void eachIntroduce() {
+		System.out.println("好きな食べ物はリンゴです");
+	}
+}
